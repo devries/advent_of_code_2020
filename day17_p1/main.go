36c9b679d111c72fa6b2154daceddbd3c90c2a9d
Christopher De Vries <devries@idolstarastronomer.com>
2020-12-17 06:51:40 -0500

    Completed day 17

---
+++ day17_p1/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/devries/advent_of_code_2020/utils"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	utils.Check(err, "error opening input")
+	defer f.Close()
+
+	grid := parseInput(f)
+	n := solve(grid)
+	fmt.Println(n)
+}
+
+func solve(grid map[Point]bool) int {
+	for i := 0; i < 6; i++ {
+		grid = step(grid)
+	}
+
+	sum := 0
+	for _, v := range grid {
+		if v {
+			sum++
+		}
+	}
+
+	return sum
+}
+
+type Point struct {
+	X int
+	Y int
+	Z int
+}
+
+func (p Point) neighbors() []Point {
+	r := make([]Point, 0, 26)
+
+	for i := p.X - 1; i <= p.X+1; i++ {
+		for j := p.Y - 1; j <= p.Y+1; j++ {
+			for k := p.Z - 1; k <= p.Z+1; k++ {
+				pn := Point{i, j, k}
+				if pn != p {
+					r = append(r, pn)
+				}
+			}
+		}
+	}
+
+	return r
+}
+
+func parseInput(r io.Reader) map[Point]bool {
+	result := make(map[Point]bool)
+
+	for j, line := range utils.ReadLines(r) {
+		for i, v := range line {
+			if v == '#' {
+				result[Point{i, j, 0}] = true
+			}
+		}
+	}
+
+	return result
+}
+
+func step(grid map[Point]bool) map[Point]bool {
+	nextGrid := make(map[Point]bool)
+
+	for k, v := range grid {
+		if v == true {
+			nextGrid[k] = false
+			for _, p := range k.neighbors() {
+				nextGrid[p] = false
+			}
+		}
+	}
+
+	for p := range nextGrid {
+		sum := 0
+		for _, np := range p.neighbors() {
+			if grid[np] == true {
+				sum++
+			}
+		}
+		if grid[p] == true && (sum == 2 || sum == 3) {
+			nextGrid[p] = true
+		} else if grid[p] == false && sum == 3 {
+			nextGrid[p] = true
+		}
+	}
+
+	return nextGrid
+}
---
+++ day17_p2/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+var testInput = `.#.
+..#
+###`
+
+func TestSolution(t *testing.T) {
+	r := strings.NewReader(testInput)
+
+	grid := parseInput(r)
+	result := solve(grid)
+	expected := 848
+	if result != expected {
+		t.Errorf("Test input: expected %d, got %d", expected, result)
+	}
+}
