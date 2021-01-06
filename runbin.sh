84726e0899f4057d5721bff928ed4503da6b82eb
Christopher De Vries <devries@idolstarastronomer.com>
2021-01-06 15:04:57 +0000

    Add some time checking to the full runbin

--- runbin.sh
+++ runbin.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+fullstart=$(date -u +%s)
 for day in $(seq -f "%02g" 1 25); do
   finecho=0
+  start=$(date -u +%s)
   for part in 1 2; do
     dir="day${day}_p${part}"
     if [ -d $dir ]; then
@@ -18,6 +20,16 @@ for day in $(seq -f "%02g" 1 25); do
     fi
   done
   if [ $finecho -eq 1 ]; then
-    echo ""
+    end=$(date -u +%s)
+    duration=$( expr $end - $start)
+    if [ $duration -eq 0 ]; then
+      printf "time elapsed: <1s\n"
+    else
+      printf "time elapsed: ${duration}s\n"
+    fi
   fi
 done
+fullend=$(date -u +%s)
+duration=$((fullend-fullstart))
+echo ""
+echo "Total time elapsed: ${duration}s"
